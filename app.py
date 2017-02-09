0f85880b47478181935cdf0788fb4afb39c20d3f
Luyun Xie <luyun@appannie.com>
2017-02-09 16:25:10 +0800

    implement /pets and /pets/<pet_id>

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+import json
+from flask import Flask, request
+from flask.views import MethodView
 
-@app.route("/")
-def index():
-    return "Hello World from producer"
+app = Flask(__name__)
+store = {}
+
+
+class Index(MethodView):
+
+    def get(self):
+        return "Hello World from PetStore"
+
+
+class Pets(MethodView):
+
+    def get(self):
+        return json.dumps(store)
+
+    def post(self):
+        pet_id = request.form['id']
+        attrs = request.form.copy()
+        del attrs['id']
+        store[pet_id] = attrs
+        return json.dumps(attrs)
+
+
+class Pet(MethodView):
+
+    def get(self, pet_id):
+        return json.dumps(store.get(pet_id))
+
+app.add_url_rule('/', view_func=Index.as_view('index'))
+app.add_url_rule('/pets', view_func=Pets.as_view('pets'))
+app.add_url_rule('/pets/<pet_id>', view_func=Pet.as_view('pet'))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
